f6ee276e5b1e88bfae355543f6f6da52c5c4986d
Ben Pintel <bpintel@gmail.com>
2022-01-13 17:05:04 -0800

    created controller methods for all get requests

--- server/controllers.js
+++ server/controllers.js
@@ -1,23 +1,72 @@
-const {} = require('./models.js');
+const { readProducts,
+  readProduct,
+  readStyles,
+  readRelated,
+  readCart } = require('./models.js');
 
 module.exports ={
 
-  getProducts: () => {
-
+  getProducts: (req, res) => {
+    const { page, count } = reg.query;
+    readProducts(product_id)
+      .then((results) => {
+        res.send(results);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
   },
   
-  getProduct: () => {
-
+  getProduct: (req, res) => {
+    const { product_id } = req.params;
+    readProduct(product_id)
+      .then((results) => {
+        res.send(results);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
   },
-  getStyles: () => {
 
+  getStyles: (req, res) => {
+    const { product_id } = req.params;
+    readStyles(product_id)
+      .then((results) => {
+        res.send(results);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
   },
-  getRelated: () => {
 
+  getRelated: (req, res) => {
+    const { product_id } = req.params;
+    readRelated(product_id)
+      .then((results) => {
+        res.send(results.rows[0]['array_agg']);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
   },
-  getCart: () => {
 
+  getCart: (req, res) => {
+    const { sku_id } = req.params;
+    readCart(sku_id)
+      .then((results) => {
+        res.send(results);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
   },
 
+  //postCart: () => {
 
+  // }
 }
